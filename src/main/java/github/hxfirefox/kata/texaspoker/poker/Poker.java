af813d68e51bb9328e9df40dd337336dfa37be8f
10110071 <huang.xiang2@zte.com.cn>
2017-08-26 23:12:02 +0800

    make poker equals test pass

--- src/main/java/github/hxfirefox/kata/texaspoker/poker/Poker.java
+++ src/main/java/github/hxfirefox/kata/texaspoker/poker/Poker.java
@@ -12,4 +12,19 @@ public class Poker {
     public static Poker card(Suit suit, int value) {
         return new Poker(suit, value);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Poker poker = (Poker) o;
+
+        return value == poker.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
 }
